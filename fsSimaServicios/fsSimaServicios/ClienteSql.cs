81d350a5edb759816960b4a22529a2a366fdb007
DRAhuja6 <drahuja6@hotmail.com>
2022-03-08 13:34:16 -0600

    Agrega paquete System.Data.OleDb. Actualiza ClienteSql.cs en fsSimaServicios.

--- fsSimaServicios/fsSimaServicios/ClienteSql.cs
+++ fsSimaServicios/fsSimaServicios/ClienteSql.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient;
+//using System.Data.SqlClient;
+using System.Data.OleDb;
 
 namespace fsSimaServicios
 {
@@ -23,11 +24,41 @@ namespace fsSimaServicios
 
         #region Métodos públicos.
 
-        public DataSet ObtenerRegistros(SqlParameter[] parametros, string storedProcedure)
+        //Este método con SqlClient deberá reemplazarse.
+        //public DataSet ObtenerRegistros(SqlParameter[] parametros, string storedProcedure)
+        //{
+        //    try
+        //    {
+        //        using (var sqlConn = new  SqlConnection(CadenaConexionDB))
+        //        {
+        //            var dataSet = new DataSet();
+        //            sqlConn.Open();
+        //            using (var sqlCommand = sqlConn.CreateCommand())
+        //            {
+        //                sqlCommand.CommandType = CommandType.StoredProcedure;
+        //                sqlCommand.CommandText = storedProcedure;
+        //                if (parametros != null)
+        //                    sqlCommand.Parameters.AddRange(parametros);
+
+        //                using (var sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+        //                {
+        //                    sqlDataAdapter.Fill(dataSet);
+        //                }
+        //            }
+        //            return dataSet;
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new DataSet();
+        //    }
+        //}
+
+        public DataSet ObtenerRegistros(OleDbParameter[] parametros, string storedProcedure)
         {
             try
             {
-                using (var sqlConn = new SqlConnection(CadenaConexionDB))
+                using (var sqlConn = new OleDbConnection(CadenaConexionDB))
                 {
                     var dataSet = new DataSet();
                     sqlConn.Open();
@@ -38,7 +69,7 @@ namespace fsSimaServicios
                         if (parametros != null)
                             sqlCommand.Parameters.AddRange(parametros);
 
-                        using (var sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+                        using (var sqlDataAdapter = new OleDbDataAdapter(sqlCommand))
                         {
                             sqlDataAdapter.Fill(dataSet);
                         }
@@ -52,11 +83,36 @@ namespace fsSimaServicios
             }
         }
 
-        public T ObtenerEscalar<T>(SqlParameter[] parametros, string storedProcedure)
+        //public T ObtenerEscalar<T>(SqlParameter[] parametros, string storedProcedure)
+        //{
+        //    try
+        //    {
+        //        using (var sqlConn = new SqlConnection(CadenaConexionDB))
+        //        {
+        //            object scalar;
+        //            sqlConn.Open();
+        //            using (var sqlCommand = sqlConn.CreateCommand())
+        //            {
+        //                sqlCommand.CommandType = CommandType.StoredProcedure;
+        //                sqlCommand.CommandText = storedProcedure;
+        //                if (parametros != null)
+        //                    sqlCommand.Parameters.AddRange(parametros);
+        //                scalar = sqlCommand.ExecuteScalar();
+        //            }
+        //            return scalar != null ? (T)scalar : default;
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return default;
+        //    }
+        //}
+
+        public T ObtenerEscalar<T>(OleDbParameter[] parametros, string storedProcedure)
         {
             try
             {
-                using (var sqlConn = new SqlConnection(CadenaConexionDB))
+                using (var sqlConn = new OleDbConnection(CadenaConexionDB))
                 {
                     object scalar;
                     sqlConn.Open();
@@ -77,11 +133,36 @@ namespace fsSimaServicios
             }
         }
 
-        public bool EjecutaProcedimiento(SqlParameter[] parametros, string storedProcedure)
+        //public bool EjecutaProcedimiento(SqlParameter[] parametros, string storedProcedure)
+        //{
+        //    try
+        //    {
+        //        using (var sqlConn = new SqlConnection(CadenaConexionDB))
+        //        {
+        //            bool result;
+        //            sqlConn.Open();
+        //            using (var sqlCommand = sqlConn.CreateCommand())
+        //            {
+        //                sqlCommand.CommandType = CommandType.StoredProcedure;
+        //                sqlCommand.CommandText = storedProcedure;
+        //                if (parametros != null)
+        //                    sqlCommand.Parameters.AddRange(parametros);
+        //                result = sqlCommand.ExecuteNonQuery() == 0;
+        //            }
+        //            return result;
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        public bool EjecutaProcedimiento(OleDbParameter[] parametros, string storedProcedure)
         {
             try
             {
-                using (var sqlConn = new SqlConnection(CadenaConexionDB))
+                using (var sqlConn = new OleDbConnection(CadenaConexionDB))
                 {
                     bool result;
                     sqlConn.Open();
@@ -107,7 +188,7 @@ namespace fsSimaServicios
             try
             {
 
-                using (var sqlConn = new SqlConnection(CadenaConexionDB))
+                using (var sqlConn = new OleDbConnection(CadenaConexionDB))
                 {
                     var dataSet = new DataSet();
                     sqlConn.Open();
@@ -116,7 +197,7 @@ namespace fsSimaServicios
                         sqlCommand.CommandType = CommandType.Text;
                         sqlCommand.CommandText = cadenaSql;
 
-                        using (var sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+                        using (var sqlDataAdapter = new OleDbDataAdapter(sqlCommand))
                         {
                             sqlDataAdapter.Fill(dataSet);
                         }
